750f19a744a45d738e89633e8a8c9cc26626724e
Rafael Dantas <rafael.d@softplan.com.br>
2019-06-27 17:47:25 -0300

    entrega inicial desafio

---
+++ dom/desafio-04/app.js
@@ -0,0 +1,31 @@
+new Vue({
+	el: '#desafio',
+	data: {
+		classe1: 'destaque',
+		perigo: true,
+		classe3: '',
+		classe4: '',
+		cor5: '',
+		estilo5: {
+			width: '100px',
+			height: '100px'
+		}
+	},
+	methods: {
+		iniciarEfeito() {
+			setInterval(() => {
+				this.classe1 = this.classe1 == 'destaque' ? 'encolher': 'destaque'
+			}, 1000)
+		},
+		iniciarProgresso() {
+
+		},
+		setPerigo(event) {
+			if (event.target.value == "true") {
+				this.perigo = true
+			} else if (event.target.value == "false") {
+				this.perigo = false
+			}
+		}
+	}
+})
