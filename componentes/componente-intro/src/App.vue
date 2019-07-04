cb92fc9672a570787a6d97be1db6ee744b94543b
Rafael Dantas <rafael.d@softplan.com.br>
2019-07-04 08:00:10 -0300

    inicio modulo componentes

---
+++ componentes/componente-intro/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <h1>Contadores</h1>
+    {{ contador }}
+  </div>    
+</template>
+
+<script>
+export default {
+  data: function() {
+    return {
+      contador: 0
+    }    
+  }
+}
+</script>
+
+<style>
+
+</style>
