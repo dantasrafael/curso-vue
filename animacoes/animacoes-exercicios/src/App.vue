611c3ef1a302a500089b19e14cad14af52e9b2e1
Rafael Dantas <rafael.d@softplan.com.br>
2019-07-11 13:42:37 -0300

    modulo animações - até animação em js

--- animacoes/animacoes-exercicios/src/App.vue
+++ animacoes/animacoes-exercicios/src/App.vue
@@ -10,7 +10,44 @@
 
 		<transition name="slide" type="animation" appear>
 			<b-alert variant="info" show v-show="exibir">{{ msg }}</b-alert>
-		</transition>		
+		</transition>	
+
+		<transition
+			enter-active-class="animated bounce"
+			leave-active-class="animated shake">
+			<b-alert variant="info" show v-show="exibir">{{ msg }}</b-alert>
+		</transition>
+
+		<hr>
+		<b-select v-model="tipoAnimacao" class="mb-4">
+			<option value="fade">Fade</option>
+			<option value="slide">Slide</option>
+		</b-select>
+		<transition :name="tipoAnimacao">
+			<b-alert variant="info" show v-show="exibir">{{ msg }}</b-alert>
+		</transition>							
+
+		<transition :name="tipoAnimacao" mode="out-in">
+			<b-alert variant="info" show v-if="exibir" key="info">{{ msg }}</b-alert>
+			<b-alert variant="info" show v-else key="warn">{{ msg }}</b-alert>
+		</transition>							
+
+		<hr>
+		<button @click="exibir2 = !exibir2">Alternar</button>
+		<transition
+			:css="false"
+			@before-enter="beforeEnter"
+			@enter="enter"
+			@after-enter="afterEnter"
+			@enter-cancelled="enterCancelled"
+			
+			@before-leave="beforeLeave"
+			@leave="leave"
+			@after-leave="afterLeave"
+			@leave-cancelled="leaveCancelled">
+			<div v-if="exibir2" class="caixa">
+			</div>
+		</transition>
 	</div>
 </template>
 
@@ -20,8 +57,51 @@ export default {
 	data() {
 		return {
 			msg: 'Uma mensagem de informação para o usuário!',
-			exibir: true
+			exibir: false,
+			exibir2: true,
+			tipoAnimacao: 'fade',
+			larguraBase: 0
 		}
+	},
+	methods: {
+		animar(el, done, negativo) {
+			let rodada = 1
+			const temporizador = setInterval(() => {
+				const novaLargura = this.larguraBase + (negativo ? -rodada * 10 : rodada * 10)
+				el.style.width = `${novaLargura}px`
+				rodada++
+				if(rodada >30) {
+					clearInterval(temporizador)
+					done()
+				}
+			}, 20)			
+		},
+		beforeEnter(el) {
+			this.larguraBase = 0
+			el.style.width = `${this.larguraBase}px`
+		},
+		enter(el, done) {
+			this.animar(el, done, false)
+		},
+		//afterEnter(el) {
+		//	console.log('afterEnter')
+		//},
+		//enterCancelled() {
+		//	console.log('enterCancelled')
+		//},
+		beforeLeave(el) {
+			this.larguraBase = 300
+			el.style.width = `${this.larguraBase}px`
+		},
+		leave(el, done) {
+			this.animar(el, done, true)
+		},
+		//afterLeave(el) {
+		//	console.log('afterLeave')
+		//},
+		//leaveCancelled() {
+		//	console.log('leaveCancelled')
+		//}
 	}
 }
 </script>
@@ -35,6 +115,13 @@ export default {
 	color: #2c3e50;
 	margin-top: 60px;
 	font-size: 1.5rem;
+}
+
+.caixa {
+	height: 100px;
+	width: 300px;
+	margin: 30px auto;
+	background-color: lightgreen;
 }
 
 .fade-enter, .fade-leave-to {
